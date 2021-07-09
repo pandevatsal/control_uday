c88a3fc6248ff7f7a10b0f7339ee11569d83c142
pandevatsal <pandevatsal.mail@gmail.com>
2021-07-09 12:47:48 +0530

    Changed header.php

--- includes/header.php
+++ includes/header.php
@@ -11,13 +11,13 @@
         <div class="collapse navbar-collapse" id="navbar">
             <ul class="nav navbar-nav navbar-right">
             <?php if (isset($_SESSION['email_id'])) { ?>
-                <li><a href="about-us.php"><span class="glyphicon glyphicon-exclamation-sign"></span> About us </a></li>
-                <li><a href="settings.php"><span class="glyphicon glyphicon-user"></span> Settings </a></li>
-                <li><a href="scripts/logout-script"><span class="glyphicon glyphicon-log-out"></span> Logout </a></li>
+                <li><a href="../about-us.php"><span class="glyphicon glyphicon-exclamation-sign"></span> About us </a></li>
+                <li><a href="../settings.php"><span class="glyphicon glyphicon-user"></span> Settings </a></li>
+                <li><a href="../scripts/logout-script"><span class="glyphicon glyphicon-log-out"></span> Logout </a></li>
             <?php } else { ?>
-                <li><a href="about-us.php"><span class="glyphicon glyphicon-exclamation-sign"></span> About us </a></li>
-                <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign up </a></li>
-                <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Log in </a></li>
+                <li><a href="../about-us.php"><span class="glyphicon glyphicon-exclamation-sign"></span> About us </a></li>
+                <li><a href="../signup.php"><span class="glyphicon glyphicon-user"></span> Sign up </a></li>
+                <li><a href="../login.php"><span class="glyphicon glyphicon-log-in"></span> Log in </a></li>
             <?php } ?>
             </ul>
         </div>
