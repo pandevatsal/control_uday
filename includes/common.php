f3d663049c482694c5431e8aea19c17b4929dbc8
pandevatsal <pandevatsal.mail@gmail.com>
2021-07-09 12:49:36 +0530

    Changed password

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "u867443879_controluday", "Curfu@241045", "u867443879_controluday") or die(mysqli_error($conn));
+    $conn = mysqli_connect("localhost", "u867443879_controluday", "", "u867443879_controluday") or die(mysqli_error($conn));
     
     if(!isset($_SESSION)){
         session_start();
